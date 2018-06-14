0f538a946dac3617c82c70a97e25e2a74896b50c
Liam <Liam@mac13r.local>
2018-06-14 10:12:51 -0400

    added to git ignore

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var loginRouter = require('./routes/login')
+var loginRouter = require('./routes/login');
+var supportRouter = require('./routes/support');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
+app.use('/support', supportRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,6 +41,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
 
 module.exports = app;
